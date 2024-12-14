f3f317c6e9f7f1558d33f5fd54b5701ce2b1cad4
harrison rogers <harrison.rogers05@gmail.com>
2024-12-15 10:53:38 +1300

    first commit

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@suiet/wallet-kit'],
+}
+
+module.exports = nextConfig
---
+++ src/providers.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { WalletProvider } from '@suiet/wallet-kit'
+import '@suiet/wallet-kit/style.css'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <WalletProvider>{children}</WalletProvider>
+}
